4fcff175fc52f28696b63452cb48cf47efe65269
Kevin Guo <Kevin@Kevins-Air.home>
2019-01-13 23:23:33 -1000

    #14

--- exercises.js
+++ exercises.js
@@ -302,6 +302,19 @@ console.log(moneyTrain(49));
  * Console.log budget and doughnutBought again.
 */ 
 
+var budget = 30;
+var doughnutPrice = 4;
+var doughnutBought = 0;
+
+function buyDoughnut(){
+  return budget -= doughnutPrice, doughnutBought++
+
+}
+buyDoughnut()
+console.log(budget, doughnutBought);
+buyDoughnut()
+console.log(budget, doughnutBought);
+
 /*Final Boss*/
 /*Create a function name dailySpecials which takes in a parameter: `special`.
 Inside the function, create a switch statement that will check the daily specials of your favorite restaurant (or make up your own daily specials for each day of the week.*/
